a891a67db8873e317de27cb77741aeb8a3271815
Adrian Le'Roy Devezin <2895303+dri94@users.noreply.github.com>
2021-09-16 21:41:29 -0400

    Quiet Drive (#2)
    * Add silent toggle
    
    * add semi colons and remove wav files
    
    * rename method
    
    * update icon and description for quietdrive
    
    (cherry picked from commit b18649e5b77712982ccc2dd31a5f9e2824f31c6f)

--- selfdrive/common/params.cc
+++ selfdrive/common/params.cc
@@ -138,6 +138,7 @@ std::unordered_map<std::string, uint32_t> keys = {
     {"CurrentRoute", CLEAR_ON_MANAGER_START | CLEAR_ON_IGNITION_ON},
     {"DisableRadar", PERSISTENT}, // WARNING: THIS DISABLES AEB
     {"EndToEndToggle", PERSISTENT},
+    {"QuietDrive", PERSISTENT},
     {"CompletedTrainingVersion", PERSISTENT},
     {"DisableDisengageOnGasToggle", PERSISTENT},
     {"DisablePowerDown", PERSISTENT},
