fdf8c49afa85ae94e3d29afffaf5047d858fcea9
sunnyhaibin <haibin.wen3@gmail.com>
2021-10-27 22:20:56 -0400

    Second try?

--- panda/board/safety/safety_subaru.h
+++ panda/board/safety/safety_subaru.h
@@ -85,13 +85,13 @@ static int subaru_rx_hook(CAN_FIFOMailBox_TypeDef *to_push) {
       update_sample(&torque_driver, torque_driver_new);
     }
 
-    if (addr == 0x240) {
-      bool acc_main_on = ((GET_BYTES_48(to_push) >> 8) & 1); // ACC main_on signal
-      if (acc_main_on && !acc_main_on_prev)
+    if (addr == 0x322) {
+      bool lkas_active = ((GET_BYTES_04(to_push) >> 24) & 1); // lkas_active signal
+      if (lkas_active && !lkas_active_prev)
       {
         controls_allowed = 1;
       }
-      acc_main_on_prev = acc_main_on;
+      lkas_active_prev = lkas_active;
     }
 
     // enter controls on rising edge of ACC, exit controls on ACC off
