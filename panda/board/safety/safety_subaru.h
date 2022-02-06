17a6b0288584ff9d3da51a9fff39d539b99c4f49
Jason Wen <haibin.wen3@gmail.com>
2022-02-06 00:48:38 -0500

    MADS - LKAS button - reset LKAS at startup - Global Subaru - 3

--- panda/board/safety/safety_subaru.h
+++ panda/board/safety/safety_subaru.h
@@ -10,7 +10,7 @@ const int SUBARU_DRIVER_TORQUE_FACTOR = 10;
 const int SUBARU_STANDSTILL_THRSLD = 20;  // about 1kph
 const uint32_t SUBARU_L_BRAKE_THRSLD = 2; // filter sensor noise, max_brake is 400
 
-const CanMsg SUBARU_TX_MSGS[] = {{0x122, 0, 8}, {0x221, 0, 8}, {0x321, 0, 8}, {0x322, 0, 8}, {0x40, 2, 8}, {0x139, 2, 8}, {0x576, 2, 8}} // ES_LKAS_State};
+const CanMsg SUBARU_TX_MSGS[] = {{0x122, 0, 8}, {0x221, 0, 8}, {0x321, 0, 8}, {0x322, 0, 8}, {0x40, 2, 8}, {0x139, 2, 8}, {0x576, 2, 8}}; // ES_LKAS_State};
 #define SUBARU_TX_MSGS_LEN (sizeof(SUBARU_TX_MSGS) / sizeof(SUBARU_TX_MSGS[0]))
 
 const CanMsg SUBARU_L_TX_MSGS[] = {{0x161, 0, 8}, {0x164, 0, 8}, {0x140, 2, 8}};
@@ -91,7 +91,7 @@ static int subaru_rx_hook(CANPacket_t *to_push) {
   if (valid && (GET_BUS(to_push) == 2U)) {
     int addr == GET_ADDR(to_push);
     if (addr == 0x576) {
-      bool lkas_pressed = (GET_BYTE(to_push, 2) & 0x4) > 0;
+      bool lkas_pressed = (GET_BYTE(to_push, 2) & 0x4) > 0; // LKAS_Dash_State signal
       if (lkas_pressed && lkas_pressed_prev) {
         controls_allowed = 1;
       }
